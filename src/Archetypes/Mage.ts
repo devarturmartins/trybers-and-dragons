7ff6b4d648e42edc7d49f88012a07375cda25601
devarturmartins <arturvl1@hotmail.com>
2023-05-09 18:38:02 -0300

    Interface Fighter e SimpleFighter, class Character

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Mage extends Archetype {
+  private static _createdArchetypeInstances = 0;
+  private _energyType: EnergyType;
+
+  constructor(name: string) {
+    super(name);
+    Mage._createdArchetypeInstances += 1;
+    this._energyType = 'mana';
+  }
+
+  static createdArchetypeInstances(): number {
+    return Mage._createdArchetypeInstances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
